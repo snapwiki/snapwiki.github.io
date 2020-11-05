318676713cb2a768c03702a12fb6255454eb0092
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-11-05 15:18:36 +0000

    style: format code

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,6 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
