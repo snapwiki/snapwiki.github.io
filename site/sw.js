eb362c808eb2086b6782bc9700d71b80e4c91b21
R4356th <R4356th@users.noreply.github.com>
2020-11-05 08:19:03 +0000

    style: format code

--- site/sw.js
+++ site/sw.js
@@ -1,34 +1,35 @@
-const staticCacheName = 'Snap! Wiki';
+const staticCacheName = "Snap! Wiki";
 const assets = [
-  'assets/js/base.js',
-  'assets/css/base.scss',
-  'assets/css/markdown.scss',
-  '/_sass/_variables.scss'
+  "assets/js/base.js",
+  "assets/css/base.scss",
+  "assets/css/markdown.scss",
+  "/_sass/_variables.scss",
 ];
 
-self.addEventListener('install', evt => {
+self.addEventListener("install", (evt) => {
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
-      console.log('Caching shell assets...');
+      console.log("Caching shell assets...");
       cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('activate', evt => {
+self.addEventListener("activate", (evt) => {
   evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName)
-        .map(key => caches.delete(key))
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key))
       );
     })
   );
 });
 
-self.addEventListener('fetch', evt => {
+self.addEventListener("fetch", (evt) => {
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
+    caches.match(evt.request).then((cacheRes) => {
       return cacheRes || fetch(evt.request);
     })
   );
