5c5519df0a97ac5fef252052db863d03f6f0ebbb
GrahamSH <64214252+GrahamSH-LLK@users.noreply.github.com>
2020-11-05 07:58:56 -0500

    Update base.js

--- site/assets/js/base.js
+++ site/assets/js/base.js
@@ -60,7 +60,4 @@ waitForElement("#dark-toggle").then(() => {
     }
   });
 });
-// Registering Service Worker
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/assets/js/sw.js");
-}
+
