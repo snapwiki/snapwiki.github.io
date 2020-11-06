d4a387850e7a2a1c5db2d08d5b9daed0b8d13870
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2020-11-06 19:28:16 +0000

    style: format code

--- site/sw.js
+++ site/sw.js
@@ -1,6 +1,8 @@
 const CACHE = "pwabuilder-offline";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -9,9 +11,9 @@ self.addEventListener("message", (event) => {
 });
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
+  new RegExp("/*"),
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
+    cacheName: CACHE,
   })
 );
 
