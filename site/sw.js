b60dac2c8efd998afac22f4b546973366082646d
GrahamSH <grahamshllk@gmail.com>
2021-03-30 09:30:20 -0400

    Update sw.js

--- site/sw.js
+++ site/sw.js
@@ -1,18 +1,22 @@
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js"
 );
+const {registerRoute} = workbox.routing;
+const {CacheFirst, StaleWhileRevalidate,NetworkFirst} = workbox.strategies;
+const {CacheableResponse} = workbox.cacheableResponse;
+const {ExpirationPlugin} = workbox.expiration;
 
 // Cache page navigations (html) with a Network First strategy
-workbox.routing.registerRoute(
+registerRoute(
   // Check to see if the request is a navigation to a new page
   ({ request }) => request.mode === "navigate",
   // Use a Network First caching strategy
-  new workbox.strategies.NetworkFirst({
+  new NetworkFirst({
     // Put all cached files in a cache named 'pages'
     cacheName: "pages",
     plugins: [
       // Ensure that only requests that result in a 200 status are cached
-      new workbox.strategies.CacheableResponsePlugin({
+      new CacheableResponse({
         statuses: [200],
       }),
     ],
@@ -20,19 +24,19 @@ workbox.routing.registerRoute(
 );
 
 // Cache CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
-workbox.routing.registerRoute(
+registerRoute(
   // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
   ({ request }) =>
     request.destination === "style" ||
     request.destination === "script" ||
     request.destination === "worker",
   // Use a Stale While Revalidate caching strategy
-  new workbox.strategies.StaleWhileRevalidate({
+  new StaleWhileRevalidate({
     // Put all cached files in a cache named 'assets'
     cacheName: "assets",
     plugins: [
       // Ensure that only requests that result in a 200 status are cached
-      new workbox.strategies.CacheableResponsePlugin({
+      new CacheableResponsePlugin({
         statuses: [200],
       }),
     ],
@@ -40,20 +44,20 @@ workbox.routing.registerRoute(
 );
 
 // Cache images with a Cache First strategy
-workbox.routing.registerRoute(
+registerRoute(
   // Check to see if the request's destination is style for an image
   ({ request }) => request.destination === "image",
   // Use a Cache First caching strategy
-  new workbox.strategies.CacheFirst({
+  new CacheFirst({
     // Put all cached files in a cache named 'images'
     cacheName: "images",
     plugins: [
       // Ensure that only requests that result in a 200 status are cached
-      new workbox.strategies.CacheableResponsePlugin({
+      new CacheableResponse({
         statuses: [200],
       }),
       // Don't cache more than 50 items, and expire them after 30 days
-      new workbox.strategies.ExpirationPlugin({
+      new ExpirationPlugin({
         maxEntries: 50,
         maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
       }),
