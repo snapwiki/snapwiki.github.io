0c6c8be58afc50e3494eab4ad8f464283b5fe56f
GrahamSH-LLK <GrahamSH-LLK@users.noreply.github.com>
2021-05-01 11:36:34 +0000

    deploy: 957bb9fce3d296b75aac312910f62277f159bca8

---
+++ assets/js/status.js
@@ -0,0 +1,1 @@
+function fetcher(){fetch("https://snapwiki.github.io/statuspage/status.json").then(t=>t.json()).then(t=>{t.status;console.log(status),document.getElementById("status").innerHTML=`${status}`})["catch"](t=>{console.error("An error occured while fetching the message count:",t)})}document.addEventListener("DOMContentLoaded",fetcher());
