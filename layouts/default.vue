a311506b28c6f15c43292d33c1775127559700d7
GrahamSH <grahamshllk@gmail.com>
2021-06-03 13:11:14 -0400

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -75,10 +75,6 @@
 
 <style>
 @import url('https://rsms.me/inter/inter.css');
-* { font-family: 'Inter', sans-serif; }
-@supports (font-variation-settings: normal) {
-  * { font-family: 'Inter var', sans-serif; }
-}
 
 
 </style>
