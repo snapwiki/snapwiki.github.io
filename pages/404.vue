1c9995b413683a2ae3b1b620438bd2cf623dc3ef
R4356th <61620631+R4356th@users.noreply.github.com>
2021-06-07 13:03:52 +0600

    Extend 404 error page

--- pages/404.vue
+++ pages/404.vue
@@ -1,3 +1,4 @@
 <template>
-<h1>404</h1>
+<h1>404 - Page Not Found</h1>
+<p>Sorry the page you requested could not be found on the server! You can return to the <a href="/">main page</a>.</p>
 </template>
