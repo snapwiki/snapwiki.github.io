23d13626fb818c9ed0a291f4ffe66a6448420dee
R4356th <61620631+R4356th@users.noreply.github.com>
2023-04-21 02:47:57 +0600

    Add note about archival

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,8 @@
       The Unofficial Snap<i>!</i> Wiki For Snappers by Snappers!
     </h1>
     The Snap<i>!</i> Wiki is an unofficial wiki about Snap!, a block-based programming language for kids and adults.
+    Please note that this GitHub Pages site, which is <em>not</em> the <a href="https://snapwiki.miraheze.org">actual wiki</a>, is not actively maintained and is being kept around for archival purposes only.
+    The status and <a href="/wiki-mirror">mirror</a> pages, however, <em>are</em> kept up to date.
   </div>
 </template>
 
